38405a9455cb0fc40b08941d14fabc28e2438c6e
rain song <e1900594321@gmail.com>
2021-09-28 01:24:53 +0800

    test of the numpy

---
+++ hellonumpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib
+
+score = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [3, 4, 5]
+])
+print(score.shape)
+print(score.ndim)
+print(score.size)
+print(score.itemsize)
+print(score.dtype)
